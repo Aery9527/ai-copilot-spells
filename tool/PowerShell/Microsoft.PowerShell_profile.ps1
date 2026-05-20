249a15e65a11b813e16eace79fc4b925280c51fb
aery <nulllifeart@gmail.com>
2026-05-20 22:11:34 +0800

    feat: pass arguments to CLI execution functions in PowerShell profile

--- tool/PowerShell/Microsoft.PowerShell_profile.ps1
+++ tool/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -13,17 +13,17 @@ function jws { # java workspace: IdeaProjects
 
 function acc {
     . "$PSScriptRoot\Set-DevEnv.ps1"
-    . "$PSScriptRoot\Exe-CC.ps1"
+    . "$PSScriptRoot\Exe-CC.ps1" @args
 }
 
 function agc {
     . "$PSScriptRoot\Set-DevEnv.ps1"
-    . "$PSScriptRoot\Exe-GC.ps1"
+    . "$PSScriptRoot\Exe-GC.ps1" @args
 }
 
 function acx {
     . "$PSScriptRoot\Set-DevEnv.ps1"
-    . "$PSScriptRoot\Exe-CX.ps1"
+    . "$PSScriptRoot\Exe-CX.ps1" @args
 }
 
 # golang cli
@@ -32,21 +32,21 @@ function gcc {
     . "$PSScriptRoot\Set-DevEnv.ps1"
     . "$PSScriptRoot\Set-GoEnv.ps1"
     . "$PSScriptRoot\Set-GoVersion.ps1"
-    . "$PSScriptRoot\Exe-CC.ps1"
+    . "$PSScriptRoot\Exe-CC.ps1" @args
 }
 
 function ggc {
     . "$PSScriptRoot\Set-DevEnv.ps1"
     . "$PSScriptRoot\Set-GoEnv.ps1"
     . "$PSScriptRoot\Set-GoVersion.ps1"
-    . "$PSScriptRoot\Exe-GC.ps1"
+    . "$PSScriptRoot\Exe-GC.ps1" @args
 }
 
 function gcx {
     . "$PSScriptRoot\Set-DevEnv.ps1"
     . "$PSScriptRoot\Set-GoEnv.ps1"
     . "$PSScriptRoot\Set-GoVersion.ps1"
-    . "$PSScriptRoot\Exe-CX.ps1"
+    . "$PSScriptRoot\Exe-CX.ps1" @args
 }
 
 # java cli
@@ -54,17 +54,17 @@ function gcx {
 function jcc {
     . "$PSScriptRoot\Set-DevEnv.ps1"
     . "$PSScriptRoot\Set-JavaVersion.ps1"
-    . "$PSScriptRoot\Exe-CC.ps1"
+    . "$PSScriptRoot\Exe-CC.ps1" @args
 }
 
 function jgc {
     . "$PSScriptRoot\Set-DevEnv.ps1"
     . "$PSScriptRoot\Set-JavaVersion.ps1"
-    . "$PSScriptRoot\Exe-GC.ps1"
+    . "$PSScriptRoot\Exe-GC.ps1" @args
 }
 
 function jcx {
     . "$PSScriptRoot\Set-DevEnv.ps1"
     . "$PSScriptRoot\Set-JavaVersion.ps1"
-    . "$PSScriptRoot\Exe-CX.ps1"
+    . "$PSScriptRoot\Exe-CX.ps1" @args
 }
