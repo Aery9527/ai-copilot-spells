32edcf1d66c1588b63203598722949fc608bfe86
aerylin <nulllifeart@gmail.com>
2026-07-31 10:28:00 +0800

    feat: update Set-GoEnv.ps1 to include GOCACHE variable setup

--- tool/PowerShell/Set-GoEnv.ps1
+++ tool/PowerShell/Set-GoEnv.ps1
@@ -2,3 +2,5 @@ $env:GO_BIN_HOME = Join-Path $HOME "go\bin"
 if ($env:PATH -notlike "*$env:GO_BIN_HOME*") {
     $env:PATH = "$env:GO_BIN_HOME;$env:PATH"
 }
+
+#$env:GOCACHE = "C:\Users\aerylin\GolandProjects\.gocache"
